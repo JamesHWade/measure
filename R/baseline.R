600d6f1bd229a662efbce144b9f5f2972848af6c
James Wade <github@jameshwade.com>
2022-08-13 21:11:03 -0400

    baseline beginnings

--- R/baseline.R
+++ R/baseline.R
@@ -1,15 +1,14 @@
 #' Title
 #'
-#' @param recipe
-#' @param ...
-#' @param role
+#' @param recipe A recipe object. The step will be added to the
+#'  sequence of operations for this recipe.
+#' @param ... One or more selector functions to choose variables
+#'  for this step.
+#' @param role Assign the role of new variables.
 #'
-#' @return
-#' @export
-#'
-#' @examples
 step_baseline <- function(recipe,
                           ...,
                           role = "predictor") {
+  cli::cli_alert_info("Not yet implemented.")
   NULL
 }
--- tests/testthat/test-baseline.R
+++ tests/testthat/test-baseline.R
@@ -1,3 +1,7 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+test_that("step baseline runs", {
+  expect_message(step_baseline())
+})
