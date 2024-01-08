a96d42c01b1d0d39838ef67680652d2ecee1786d
James Wade <github@jameshwade.com>
2024-01-08 13:21:49 -0500

    update snapshops, cleanup description and namespace

--- R/helpers.R
+++ R/helpers.R
@@ -58,3 +58,22 @@ check_for_measure <- function(x) {
   }
   invisible(NULL)
 }
+
+# ------------------------------------------------------------------------------
+# Make sue that data are in the correct format.
+
+check_has_measure <- function(x, cl) {
+  step_fn <- as.character(cl[[1]])
+  step_fn <- gsub("prep\\.", "", step_fn)
+  step_fn <- paste0("`", step_fn, "()`.")
+
+
+  if (!any(names(x) == ".measures")) {
+    msg <-
+      paste0("It appears that the measurements have not been converted ",
+             "for the inernal format. See `step_measure_input_long()` ",
+             "and `step_measure_input_wide()` and use these prior to ",
+             step_fn)
+    rlang::abort(msg)
+  }
+}
