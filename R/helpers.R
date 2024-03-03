1129f608befa873f8aae0466e314e6b179b5a55e
James Wade <github@jameshwade.com>
2024-03-03 08:36:46 -0500

    add check for measure_obj class helper fn check_is_measure

--- R/helpers.R
+++ R/helpers.R
@@ -63,12 +63,7 @@ check_for_measure <- function(x) {
 # Make sue that data are in the correct format.
 
 check_has_measure <- function(x, cl) {
-  step_fn <- as.character(cl[[1]])
-  step_fn <- gsub("prep\\.", "", step_fn)
-  step_fn <- paste0("`", step_fn, "()`.")
-
-
-  if (!any(names(x) == ".measures")) {
+  if (!any(inherits(x, "measure_obj"))) {
     msg <-
       paste0(
         "It appears that the measurements have not been converted ",
